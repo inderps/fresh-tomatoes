ae30604bbcac7925eeba60987b1272dadae97f32
Inderpal Singh <Inderpal@IPSmac-2.local>
2012-10-17 23:50:22 +0530

    Rails skeleton added with pg database <IPS>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FreshTomatoes::Application.config.secret_token = '4b7f4fa686d9a41bba056fddb3ee6da4ce0c94a3b61fab23a9d67dbc87bf0375471d948e372d2cf4aa54d973c057fa0ef7981dd7257e5c5f94de18bc37551b88'
